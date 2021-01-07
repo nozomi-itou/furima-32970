b89809389f887e4c8333ff3228c29d218babd52c
nozomi-itou <75615242+nozomi-itou@users.noreply.github.com>
2021-01-07 21:53:16 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -39,11 +39,14 @@ class ItemsController < ApplicationController
 
   def destroy
     if @item.user_id == current_user.id
-      @item.destroy
+      if @item.destroy
+        redirect_to root_path
+      else
+        render :show
+      end
     else
-      redirect_to action: :show
+      redirect_to root_path
     end
-    redirect_to root_path
   end
 
 
