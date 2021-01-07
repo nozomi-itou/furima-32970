7bf440eebd1eae95a8b250fce54340fd307bebee
nozomi-itou <75615242+nozomi-itou@users.noreply.github.com>
2021-01-07 11:33:11 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,10 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:/, :new]
-  before_action :move_to_index, except: [:index, :show]
 
-  def index
-    @items = Item.includes(:user).order("created_at DESC")
-  end
+
+  #def index
+    #@items = Item.includes(:user).order("created_at DESC")
+  #end
 
   def new
     @item = Item.new
@@ -25,10 +25,5 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:name, :details, :category_id, :status_id, :postage_id, :area_id, :day_id, :price, :image).merge(user_id: current_user.id)
   end
 
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
-  end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -21,5 +21,5 @@ class Item < ApplicationRecord
   validates :category_id, :status_id, :postage_id, :area_id, :day_id, numericality: { other_than: 1 } 
 
   validates  :price,  presence: true, format: {with: /\A[0-9]+\z/}, numericality: { only_integer: true,
-    greater_than: 300, less_than: 10000000}
+    greater_than: 299, less_than: 10000000}
 end
